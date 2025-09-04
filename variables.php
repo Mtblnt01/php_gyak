e8c03ad332ebf53ff3a2ebf33cf9295be96eb8ef
Mtblnt01 <balintmate963@gmail.com>
2025-09-04 10:48:43 +0200

    variables.php

---
+++ variables.php
@@ -0,0 +1,15 @@
+<?php
+    /*
+    Szükséges programok: XAMPP (Apache, MySql), Git, Github repo, VSC
+    <?php ... ?>
+    
+    
+    */
+    $name = "mozso";
+    $age = 18;
+    $city = "Kisújszállás";
+
+    var_dump($name);
+    echo "Név: $name Életkor: $age Lakóhely: $city";
+
+?>
