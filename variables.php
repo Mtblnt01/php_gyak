87240a42dd6add21358abee3a787deb719d5af09
Mtblnt01 <balintmate963@gmail.com>
2025-09-04 11:22:32 +0200

    variables.php kesz

--- variables.php
+++ variables.php
@@ -10,6 +10,15 @@
     $city = "Kisújszállás";
 
     var_dump($name);
-    echo "Név: $name Életkor: $age Lakóhely: $city";
+    echo "\n<br>\nNév: $name \nÉletkor: $age \nLakóhely: $city";
+    //konstans
+    define("PI", 3.14);
+    echo "<br>", PI;
 
+    //adattípusok: string, int, float, bool, array,
+    $message = "1";
+    echo "Kiír", $message, "értéket.", "<br>\n";
+    echo "Kiír $message értéket.", "<br>\n";
+    echo "Kiír {$message}darab értéket.", "<br>\n";
+    echo 'Kiír {$message}darab értéket.', "<br>\n";
 ?>
