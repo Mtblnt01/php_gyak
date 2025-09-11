cb748d74a5028ac66d8f5bc682beb1d6a460665d
Mtblnt01 <balintmate963@gmail.com>
2025-09-11 08:12:40 +0200

    functions done

---
+++ functions.php
@@ -0,0 +1,22 @@
+<?php
+/*
+    fuggveny definiálása, paraméterek, visszatérési érték
+
+
+*/
+//Írj egy fgv-t ami visszaadja két szám összegét
+function sum(int $a, int $b): int {
+    return $a + $b;
+}
+
+$s1 = sum(5, 3);
+echo "{$s1} <br>";
+$s2 = sum("4", "6");
+echo $s2;
+
+function sayHello($name="Guest"){
+    return "Hello, $name!";
+}
+
+echo sayHello("Mozso");
+?>
