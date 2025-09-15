adfa9e0ebc2a957ced4a06bf958f7aff9ffc2755
Máté Bálint <1tc-matbal@moriczref.lan>
2025-09-15 08:08:11 +0200

    a kezdetek

---
+++ get_post.php
@@ -0,0 +1,19 @@
+<?php?>
+//egy urlap amibe egy darab mező van, kérem a nevet és ottvan mögötte a submit gomb
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>űrlapkezelés</h1>
+    <form action="" method="post">
+        <label for="name">Név:</label>
+        <input type="text" name="name" id="name">
+        <button type="submit">Küld</button>
+    </form>
+</body>
+</html>
+
