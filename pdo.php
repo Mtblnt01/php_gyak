62a51e6f37220021c079d8a38d9cc34393fc525d
Mtblnt01 <balintmate963@gmail.com>
2025-09-29 09:29:11 +0200

    0929elso2ora pdo.php folytatasa kesz

--- pdo.php
+++ pdo.php
@@ -33,8 +33,41 @@ $pass = '';
 
 try {
     $pdo = new PDO($dsn, $user, $pass);
+
+    //Hiba mód: Exception dobása hiba esetén
+    $pdo->SetAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+    echo "Sikeres csatlakozás";
+
+    $name = "pumpal";
+    $companyName = "";
+    $phone = "1241243";
+    $email = "pumpal@mzsrk.hu";
+    $photo = null;
+    //$status =?;
+    $note = "webfejlesztő";
+
+    //$sql = "INSERT INTO cards(`name`, `companyName`, `phone`, `email`, `photo`, `note`) VALUES ('$name', '$companyName', '$phone', '$email', '$photo', '$note')";
+    //$pdo->exec($sql);
+
+    $sql = "INSERT INTO cards(`name`, `companyName`, `phone`, `email`, `photo`, `note`) 
+        VALUES (?, ?, ?, ?, ?, ?)";
+
+    $stmt = $pdo->prepare($sql);
+    $stmt -> execute([$name, $companyName, $phone, $email, $photo, $note]);
+
+
+    $sql = "SELECT * FROM cards WHERE id = 11";
+    $result = $pdo->query($sql);
+    $card=$result->fetch(PDO::FETCH_ASSOC);
+    echo "<br>";
+    print_r($card);
+
+
+
 } catch (PDOException $ex) {
     echo "Kapcsolódási hiba: {$ex->getMessage()}.";
+    exit();
 }
 
 
