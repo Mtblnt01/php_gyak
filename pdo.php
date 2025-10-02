306d0fd80143305878c204b56ba6b610511469be
Mtblnt01 <balintmate963@gmail.com>
2025-10-02 08:47:33 +0200

    sql_injection és xss elleni védelem

--- pdo.php
+++ pdo.php
@@ -38,32 +38,12 @@ try {
     $pdo->SetAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
     echo "Sikeres csatlakozás";
+    //xss
+    //xss($pdo);
 
-    $name = "pumpal";
-    $companyName = "";
-    $phone = "1241243";
-    $email = "pumpal@mzsrk.hu";
-    $photo = null;
-    //$status =?;
-    $note = "webfejlesztő";
-
-    //$sql = "INSERT INTO cards(`name`, `companyName`, `phone`, `email`, `photo`, `note`) VALUES ('$name', '$companyName', '$phone', '$email', '$photo', '$note')";
-    //$pdo->exec($sql);
-
-    $sql = "INSERT INTO cards(`name`, `companyName`, `phone`, `email`, `photo`, `note`) 
-        VALUES (?, ?, ?, ?, ?, ?)";
-
-    $stmt = $pdo->prepare($sql);
-    $stmt -> execute([$name, $companyName, $phone, $email, $photo, $note]);
-
-
-    $sql = "SELECT * FROM cards WHERE id = 11";
-    $result = $pdo->query($sql);
-    $card=$result->fetch(PDO::FETCH_ASSOC);
-    echo "<br>";
-    print_r($card);
-
-
+    //sql_injection($pdo);
+    //prepared_statement($pdo); //védekezés sql injection ellen
+    checked_insert($pdo);//minden mezohoz adjunk meg egy stringet és ezt sql injection ellen védve és xsstol vedve adjuk hozza az adatbazishoz
 
 } catch (PDOException $ex) {
     echo "Kapcsolódási hiba: {$ex->getMessage()}.";
@@ -71,7 +51,71 @@ try {
 }
 
 
+function xss($pdo){
+    // INSERT
+    $name = "pumpal";
+    $companyName = htmlspecialchars("<script>alert(\"hacked\")</script>");
+    $phone = "1241243";
+    $email = "pumpal@mzsrk.hu";
+    $photo = null;
+    //$status =?;
+    $note = "webfejlesztő";
+
+    $sql = "INSERT INTO cards(`name`, `companyName`, `phone`, `email`, `photo`, `note`) VALUES ('$name', '$companyName', '$phone', '$email', '$photo', '$note')";
+    $pdo->exec($sql);
+
+    // READ
+    $sql = "SELECT * FROM cards WHERE name = 'pumpal'";
+    $result = $pdo->query($sql);
+    $card=$result->fetch(PDO::FETCH_ASSOC);
+    echo "<br>";
+    print_r($card);
+}
+
+function sql_injection($pdo){
+    $name_i = "' OR '1' = '1"; // támadó kód
+    $sql = "SELECT * FROM cards WHERE name = '$name_i'";
+    $result = $pdo->query($sql);
+    $card=$result->fetchAll(PDO::FETCH_ASSOC);
+    echo "<br>";
+    print_r($card);
+}
+
+function prepared_statement($pdo){
+    $name_i = "' OR '1' = '1"; // támadó kód
+    $sql = "SELECT * FROM cards WHERE name = ?";
+    $stmt = $pdo->prepare($sql);
+    $stmt -> execute([$name_i]);
+
+    $card= $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo "<br>";
+    print_r($card);
+
+    /*$sql = "INSERT INTO cards(`name`, `companyName`, `phone`, `email`, `photo`, `note`) VALUES ('name', '$companyName', '$phone', '$email', '$photo', '$note')";
+    $pdo->exec($sql);
+
+    // READ
+    $sql = "SELECT * FROM cards WHERE name = 'pumpal'";
+    $result = $pdo->query($sql);
+    $card=$result->fetch(PDO::FETCH_ASSOC);
+    echo "<br>";
+    print_r($card);*/
+}
+
+function checked_insert($pdo){
+    
+    $name = htmlspecialchars("valami");
+    $companyName = htmlspecialchars("amerika");
+    $phone = htmlspecialchars("06307539397");
+    $email = htmlspecialchars("valami@amerika.com");
+    $photo = null;
+    $status = null;
+    $note = htmlspecialchars("sziatesominmdenokescsa");
 
 
+    $sql = "INSERT INTO cards(`name`, `companyName`, `phone`, `email`, `photo`, `note`) VALUES (?, ?, ?, ?, ?, ?)";
+    $stmt = $pdo->prepare($sql);
+    $stmt -> execute([$name, $companyName, $phone, $email, $photo, $note]);
+}
 
 ?>
